69fdcc0f3e96c8132b7193080e2d83f4c184f0a3
YutingXia006 <128974737+YutingXia006@users.noreply.github.com>
2026-05-08 22:06:27 +0200

    fixed caching problem

--- src/retriever.py
+++ src/retriever.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+import streamlit as st
 
 ROOT = Path(__file__).parent.parent
 FAISS_INDEX = ROOT / "data" / "faiss_index"
 
+@st.cache_resource
 def load_retriever():
     embeddings = HuggingFaceEmbeddings(
         model_name="ibm-granite/granite-embedding-278m-multilingual"
