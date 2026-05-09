80b92316f24a2663f840187877a32c7367ad89ea
YutingXia006 <128974737+YutingXia006@users.noreply.github.com>
2026-05-09 19:41:37 +0200

    fix cache problems in streamlit cloud

--- src/retriever.py
+++ src/retriever.py
@@ -1,20 +1,24 @@
 from pathlib import Path
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+import streamlit as st
 
 ROOT = Path(__file__).parent.parent
 FAISS_INDEX = ROOT / "data" / "faiss_index"
 
+@st.cache_resource
 def load_retriever():
     embeddings = HuggingFaceEmbeddings(
-        model_name="ibm-granite/granite-embedding-278m-multilingual"
+        model_name="ibm-granite/granite-embedding-278m-multilingual",
+        model_kwargs={"device": "cpu"},
+        encode_kwargs={"normalize_embeddings": True}
     )
     vectorstore = FAISS.load_local(
         str(FAISS_INDEX),
         embeddings,
         allow_dangerous_deserialization=True
     )
-    return vectorstore.as_retriever(search_kwargs={"k": 5})
+    return vectorstore.as_retriever(search_kwargs={"k": 3})
 
 if __name__ == "__main__":
     retriever = load_retriever()
